2c14676d50d7eff01fcab9b995a6d9bde8c620c9
bhasevambit <jammadoibit6123@gmail.com>
2023-07-22 23:06:20 +0900

    devide plot time and freq graph func

---
+++ modules/plot_waveform_and_freq_response.py
@@ -0,0 +1,83 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+import warnings
+
+
+def plot_waveform_and_freq_response(
+        fig,
+        wave_fig,
+        freq_fig,
+        data_normalized,
+        t,
+        amp_normalized,
+        freq,
+        plot_pause,
+        view_range,
+        dbref,
+        A
+):
+    # =======================================================
+    # === Microphone入力音声ストリームデータ プロット関数 ===
+    # =======================================================
+    # fig : リアルタイム更新対象のグラフfigure
+    # wave_fig : リアルタイム更新対象の時間領域波形グラフfigure
+    # freq_fig : リアルタイム更新対象の周波数特性グラフfigure
+    # data_normalized : 時間領域 波形データ(正規化済)
+    # t : 時間領域 X軸向けデータ [ms]
+    # amp_normalized : 周波数特性 振幅データ(正規化済)
+    # freq : 周波数特性 X軸向けデータ
+    # fs : フレームサイズ [sampling data count/frame]
+    # plot_pause : グラフリアルタイム表示のポーズタイム [s]
+    # view_range : 時間領域波形グラフ X軸表示レンジ [sample count]
+    # dbref : デシベル基準値
+    # A : 聴感補正(A特性)の有効(True)/無効(False)設定
+
+    # フォント種別、およびサイズ設定
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['font.family'] = 'Times New Roman'
+
+    # 目盛内側化
+    plt.rcParams['xtick.direction'] = 'in'
+    plt.rcParams['ytick.direction'] = 'in'
+
+    # 時間領域波形 軸ラベル設定
+    wave_fig.set_xlabel('Time [ms]')
+    wave_fig.set_ylabel('Amplitude')
+
+    # 周波数特性 軸ラベル設定
+    freq_fig.set_xlabel('Frequency [Hz]')
+    if (dbref > 0) and not (A):
+        freq_fig.set_ylabel('Amplitude [dB spl]')
+    elif (dbref > 0) and (A):
+        freq_fig.set_ylabel('Amplitude [dB spl(A)]')
+    else:
+        freq_fig.set_ylabel('Amplitude')
+
+    # 時間領域波形 軸目盛り設定
+    wave_fig.set_xlim(0, view_range)
+    wave_fig.set_ylim(-1, 1)
+    wave_fig.set_yticks([-1, -0.5, 0, 0.5, 1])
+
+    # 周波数特性 軸目盛り設定
+    freq_fig.set_xlim(0, 5000)
+    freq_fig.set_ylim(-10, 90)
+    freq_fig.set_yticks(np.arange(0, 100, 20))
+
+    # plot.figure.tight_layout()実行時の「UserWarning: The figure layout has
+    # changed to tight」Warning文の抑止
+    warnings.simplefilter('ignore', UserWarning)
+
+    # レイアウト設定
+    fig.tight_layout()
+
+    # 時間領域波形グラフプロット
+    wave_fig.plot(t, data_normalized, color="blue")
+
+    # 周波数特性グラフプロット
+    freq_fig.plot(freq, amp_normalized, color="dodgerblue")
+
+    plt.pause(plot_pause)
+
+    freq_fig.cla()
+    wave_fig.cla()
--- pyaudio_Realtime-Plot_Microphone-Input.py
+++ pyaudio_Realtime-Plot_Microphone-Input.py
@@ -1,89 +1,13 @@
-import numpy as np
 from matplotlib import pyplot as plt
 
 import platform
-import warnings
 
 from modules.get_mic_index import get_mic_index
 from modules.audio_stream import audio_stream_start
 from modules.audio_stream import audio_stream_stop
 from modules.gen_time_domain_data import gen_time_domain_data
 from modules.gen_freq_domain_data import gen_freq_domain_data
-
-
-def plot_waveform_and_freq_response(
-        data_normalized,
-        t,
-        amp_normalized,
-        freq,
-        plot_pause,
-        view_range,
-        dbref,
-        A
-):
-    # =======================================================
-    # === Microphone入力音声ストリームデータ プロット関数 ===
-    # =======================================================
-    # data_normalized : 時間領域 波形データ(正規化済)
-    # t : 時間領域 X軸向けデータ [ms]
-    # amp_normalized : 周波数特性 振幅データ(正規化済)
-    # freq : 周波数特性 X軸向けデータ
-    # fs : フレームサイズ [sampling data count/frame]
-    # plot_pause : グラフリアルタイム表示のポーズタイム [s]
-    # view_range : 時間領域波形グラフ X軸表示レンジ [sample count]
-    # dbref : デシベル基準値
-    # A : 聴感補正(A特性)の有効(True)/無効(False)設定
-
-    # フォント種別、およびサイズ設定
-    plt.rcParams['font.size'] = 14
-    plt.rcParams['font.family'] = 'Times New Roman'
-
-    # 目盛内側化
-    plt.rcParams['xtick.direction'] = 'in'
-    plt.rcParams['ytick.direction'] = 'in'
-
-    # 軸ラベル設定
-    plt.xlabel('Sample Count')
-    plt.ylabel('Amplitude')
-    wave_fig.set_xlabel('time[s]')
-    wave_fig.set_ylabel('Amplitude')
-    fft_fig.set_xlabel('Frequency [Hz]')
-
-    if (dbref > 0) and not (A):
-        fft_fig.set_ylabel('Amplitude [dB spl]')
-    elif (dbref > 0) and (A):
-        fft_fig.set_ylabel('Amplitude [dB spl(A)]')
-    else:
-        fft_fig.set_ylabel('Amplitude')
-
-    # スケール設定
-    wave_fig.set_xlim(0, view_range)
-    wave_fig.set_ylim(-1, 1)
-    wave_fig.set_yticks([-1, -0.5, 0, 0.5, 1])
-
-    # fft_fig.set_xscale("log")
-    fft_fig.set_xlim(0, 5000)
-    fft_fig.set_ylim(-10, 90)
-    fft_fig.set_yticks(np.arange(0, 100, 20))
-
-    # plot.figure.tight_layout()実行時の「UserWarning: The figure layout has
-    # changed to tight」Warning文の抑止
-    warnings.simplefilter('ignore', UserWarning)
-
-    # レイアウト設定
-    fig.tight_layout()
-
-    # 時間領域波形グラフプロット
-    wave_fig.plot(t, data_normalized, color="blue")
-
-    # 周波数特性グラフプロット
-    fft_fig.plot(freq, amp_normalized, color="dodgerblue")
-
-    plt.pause(plot_pause)
-
-    fft_fig.cla()
-    wave_fig.cla()
-
+from modules.plot_waveform_and_freq_response import plot_waveform_and_freq_response
 
 if __name__ == '__main__':
     # =================
@@ -124,7 +48,7 @@ if __name__ == '__main__':
     # === 時間領域波形と周波数特性向けの2つのグラフ領域を作成
     fig = plt.figure()
     wave_fig = fig.add_subplot(2, 1, 1)
-    fft_fig = fig.add_subplot(2, 1, 2)
+    freq_fig = fig.add_subplot(2, 1, 2)
 
     # === 時間領域波形 & 周波数特性リアルタイムプロット ===
     # キーボードインタラプトあるまでループ処理継続
@@ -146,6 +70,9 @@ if __name__ == '__main__':
 
             # === グラフプロット ===
             plot_waveform_and_freq_response(
+                fig,
+                wave_fig,
+                freq_fig,
                 data_normalized,
                 t,
                 amp_normalized,
